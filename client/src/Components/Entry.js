3541318eb24874afff0b36295c241fcb238c2d78
Matthew Fan <kungfumo@gmail.com>
2021-03-26 20:50:31 -0400

    corrected number of photos showing in profile

--- client/src/Components/Entry.js
+++ client/src/Components/Entry.js
@@ -17,20 +17,34 @@ const Post = ({ title, description }) => {
 }
 
 const Gallery = ({ caption, photos}) => {
+  const getDisplayedPhotos = () => {
+    if (photos.length > 4) {
+      if (photos.length == 5) {
+        return photos
+      } else {
+        return photos.slice(0, 4)
+      }
+    } else {
+      return photos
+    }
+  }
+
   return (
     <div>
       <div className="mb-4">{caption}</div>
       <div className="grid xs:grid-cols-5 grid-cols-4 gap-2">
-        {photos.slice(0,4).map(photo => (
+        {getDisplayedPhotos().map(photo => (
           <div style={{'backgroundImage': `url(https://picsum.photos/200/300?random={photo})`}} className="image-card relative bg-cover">
           </div>
         ))}
+        {photos.length > 5 ? (
         <div className="image-card">
           <div className="absolute inset-0 flex flex-col justify-center items-center">
             <FontAwesomeIcon icon={["far", "images"]} size="2x" />
-            <div className="break-all mt-2"><span className="font-semibold">3</span> more</div>
+            <div className="break-all mt-2"><span className="font-semibold">{photos.length - 4}</span> more</div>
           </div>
         </div>
+        ) : ""}
       </div>
     </div>
   )
--- client/src/Views/Profile.js
+++ client/src/Views/Profile.js
@@ -38,6 +38,29 @@ const Profile = () => {
           caption="food excursions in nyc"
           photos={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
         />
+        <Entry
+          key={3}
+          username="mattfan00"
+          date="Mar 21, 2021"
+          type="post"
+          title="one year"
+          description="I first started coding back in 8th grade when I took a programming course offered by my school and learned Visual Basic. After that I learned some HTML, CSS, and Javascript (copy and pasting from Stack Overflow) in highschool..."
+        />
+        <Entry
+          key={2}
+          username="mattfan00"
+          date="Mar 21, 2021"
+          type="thought"
+          content="graduation is coming up! excited for my new life after school, but not excited to leave my old one"
+        />
+        <Entry
+          key={4}
+          username="mattfan00"
+          date="Mar 21, 2021"
+          type="gallery"
+          caption="food excursions in nyc"
+          photos={[1, 2, 3, 4]}
+        />
       </div>
     </div>
   )
