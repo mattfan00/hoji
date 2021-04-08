b916576e39fb86752523e4ba38e171b5e99cfa82
Matthew Fan <kungfumo@gmail.com>
2021-04-08 12:59:50 -0400

    cleaned up mock entries and added settings button to profile page

--- client/src/Components/ProfileHeader.js
+++ client/src/Components/ProfileHeader.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from "react"
+import { Link } from "react-router-dom"
 import Button from "../Components/Button"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import DefaultProPic from "../Icons/DefaultProPic"
@@ -18,23 +19,32 @@ const ProfileHeader = ({
 
   return (
     <div className="card mb-16">
-      <div className="flex items-center mb-4">
+      <div className="flex items-center">
         <div className="mr-4 w-10 h-10 rounded-full overflow-hidden">
           <DefaultProPic />
         </div>
         <div className="flex-1 flex justify-between items-center">
           <div className="flex flex-col">
             <h3 className="mb-0">{name}</h3>
-            <div className="leading-3">@{username}</div>
+            <div className="leading-4">@{username}</div>
           </div>
           {user && user.username !== username ? (
           <Button onClick={toggleBookmark}>
             <FontAwesomeIcon icon={[bookmark ? "fas" : "far", "bookmark"]} size="lg" />
           </Button>
           ) : ""}
+          {user && user.username === username ? (
+          <Link to="/settings">
+            <Button onClick={toggleBookmark}>
+              <FontAwesomeIcon icon="cog" size="lg" />
+            </Button>
+          </Link>
+          ) : ""}
         </div>
       </div>
-      <div>{description}</div>
+      {description ? (
+      <div className="mt-4">{description}</div>
+      ): ""}
     </div>
   )
 }
--- client/src/Views/Profile.js
+++ client/src/Views/Profile.js
@@ -67,60 +67,6 @@ const Profile = () => {
             />
           </div>
         ))}
-
-        <Entry
-          key={1}
-          author="mattfan00"
-          created="Mar 21, 2021"
-          type="thought"
-          content="why is collaborative work so hard"
-        />
-        <Entry
-          key={2}
-          author="mattfan00"
-          created="Mar 21, 2021"
-          type="thought"
-          content="graduation is coming up! excited for my new life after school, but not excited to leave my old one"
-        />
-        <Entry
-          key={3}
-          author="mattfan00"
-          created="Mar 21, 2021"
-          type="post"
-          title="one year"
-          description="I first started coding back in 8th grade when I took a programming course offered by my school and learned Visual Basic. After that I learned some HTML, CSS, and Javascript (copy and pasting from Stack Overflow) in highschool..."
-        />
-        <Entry
-          key={4}
-          author="mattfan00"
-          created="Mar 21, 2021"
-          type="gallery"
-          content="food excursions in nyc"
-          photos={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
-        />
-        <Entry
-          key={5}
-          author="mattfan00"
-          created="Mar 21, 2021"
-          type="post"
-          title="one year"
-          description="I first started coding back in 8th grade when I took a programming course offered by my school and learned Visual Basic. After that I learned some HTML, CSS, and Javascript (copy and pasting from Stack Overflow) in highschool..."
-        />
-        <Entry
-          key={6}
-          author="mattfan00"
-          created="Mar 21, 2021"
-          type="thought"
-          content="graduation is coming up! excited for my new life after school, but not excited to leave my old one"
-        />
-        <Entry
-          key={7}
-          author="mattfan00"
-          created="Mar 21, 2021"
-          type="gallery"
-          content="food excursions in nyc"
-          photos={[1, 2, 3, 4]}
-        />
       </div>
     </div>
   )
